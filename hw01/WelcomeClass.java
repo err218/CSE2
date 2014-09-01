d97e3042846c5e4756444fdaec99d430fc2eca95
err218 <err218@lehigh.edu>
2014-09-01 15:10:06 +0000

    Updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,27 @@
+//Emma Resnick
+//CSE2
+//30 August 2014
+//hw01
+
+
+
+//class
+public class WelcomeClass{
+    
+    //main method
+    public static void main(String[] args){
+        
+        //statement
+        System.out.println("----------");
+        System.out.println("| WELCOME |");
+        System.out.println("----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^ ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-E--R--R--2--1--8-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v ");
+        System.out.println("My name is Emma and I am from Springfield, NJ.");
+        System.out.println("This summer, I was a camp counselor at a day camp.");
+        
+    }
+}
