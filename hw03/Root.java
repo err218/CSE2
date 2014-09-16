d917e5d8f94da52b401ee30cc20f3144f1f26809
err218 <err218@lehigh.edu>
2014-09-16 17:12:48 +0000

    hw03

--- hw03/Root.java
+++ hw03/Root.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 public class Root{
     public static void main(String [] arg){
-        
+        //define scanner
         Scanner myScanner;
         
         myScanner =new Scanner (System.in);
@@ -25,9 +25,10 @@ public class Root{
         double guess5 =((2*guess4*guess4*guess4+xValue)/(3*guess4*guess4));
         double guess6=((2*guess5*guess5*guess5+xValue)/(3*guess5*guess5));
         
-        //show guess5 cubed to see if guess was accurate
+        //show guess6 cubed to see if final guess was accurate
         double guess6Cubed=guess6*guess6*guess6;
         
+        //prints out each guess
         System.out.println("The first guess of the cube root is: " + guess1);
         System.out.println("The second guess of the cube root is: " + guess2);
         System.out.println("The third guess of the cube root is: " + guess3);
@@ -35,7 +36,7 @@ public class Root{
         System.out.println("The fifth guess of the cube root is: " + guess5);
         System.out.println("The sixth guess of the cube root is: " + guess6);
         
-        
+        //prints out final guess and then prints out the final guess cubed
         System.out.println("The cube root is: "+ guess6);
         System.out.println(guess6+"*"+guess6+"*"+guess6+ "="+ guess6Cubed);
         
